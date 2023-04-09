facc51d189dcba7c575eefe8055ac91078d374da
LijaTureca <124587378+LijaTureca@users.noreply.github.com>
2023-04-09 12:33:49 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,21 +1,43 @@
 # python3
 
-def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    command=input()
-    if 'I' in command:
-        p=input()
-        line=input()
+# def read_input():
+#     # this function needs to aquire input both from keyboard and file
+#     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+#     command=input()
+#     if 'I' in command:
+#         p=input()
+#         line=input()
 
-    if 'F' in command:
-        file=input()
-        name="test/"+file
-        with open(name,"r") as file:
-                p=file.readline().rstrip()
-                line=file.readline().rstrip()
+#     if 'F' in command:
+#         file=input()
+#         name="test/"+file
+#         with open(name,"r") as file:
+#                 p=file.readline().rstrip()
+#                 line=file.readline().rstrip()
     
-    return (p, line)
+#     return (p, line)
+
+def read_input():
+    # this function needs to acquire input both from keyboard and file
+    # as before, use capital i (input from keyboard) and capital f (for input from file)
+
+    input_type = input().strip().upper()
+
+    if input_type == 'I':
+        p = input().strip()
+        line = input().strip()
+
+    elif input_type == 'F':
+        filename = input().strip()
+        with open(f"test/{filename}") as file:
+            p = file.readline().strip()
+            line = file.readline().strip()
+
+    else:
+        print("Invalid input type. Please choose either 'I' for input from keyboard or 'F' for input from file.")
+        exit()
+
+    return p, line
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
